bce525bb8cf001ac19d2a0ece84c99b67b58cb52
Nadja2310 <maksakova.nadja@gmail.com>
2020-12-08 16:44:55 +0100

    tasks 3,4

--- 02_12_2020/src/ArrayInt/ArrayInt.java
+++ 02_12_2020/src/ArrayInt/ArrayInt.java
@@ -18,10 +18,48 @@ public class ArrayInt {
             if (element.getValue() > max) {
                 max = element.getValue();
                 inputValue = element.getKey();
-            } else if (element.getValue()==max)
-                inputValue=element.getKey();
+            } else if (element.getValue() == max)
+                inputValue = element.getKey();
         }
 
         return inputValue;
+    }
+
+    public boolean switchArray(int[] array) {
+        int evenElement = array[0];
+        int oddElement = array[1];
+        int countEven = 1;
+        int countOdd = 1;
+        int eventElementLenght = (array.length % 2 == 0 ? (array.length / 2) : (array.length / 2 + 1));
+        for (int i = 2; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] == evenElement)
+                countEven++;
+            if (i % 2 != 0 && array[i] == oddElement)
+                countOdd++;
+        }
+
+        return (countOdd == (array.length / 2) && countEven == (eventElementLenght));
+    }
+
+    public int maxSpan(int[] arr) {
+
+        int maxSpan = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int element = arr[i];
+            int outputLength = 0;
+
+            for (int j = arr.length - 1; j >= 0; j--) {
+                if (element == arr[j]) {
+                    outputLength = j - i + 1;
+                    break;
+                }
+            }
+            maxSpan = Math.max(outputLength, maxSpan);
+        }
+        if (arr.length > 1 && maxSpan == 1)
+            maxSpan = 2;
+
+        return maxSpan;
     }
 }
--- 02_12_2020/src/ArrayInt/ArrayIntTest.java
+++ 02_12_2020/src/ArrayInt/ArrayIntTest.java
@@ -7,16 +7,60 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArrayIntTest extends ArrayInt {
 
     @Test
-    void testarrayInt(){
+    void testarrayInt_OneValue_true(){
         ArrayInt arr=new ArrayInt();
         int[] arrayInt=new int[]{1,1,3,1,1,2,3,1};
-
         assertEquals(1,arr.searchFrequentElement(arrayInt));
-        int[] arrayOneElement=new int[]{8,7,2,3,4};
-        assertEquals(8,arr.searchFrequentElement(arrayOneElement));
+    }
+    @Test
+    void testArrayInt_AllValue_true(){
+        ArrayInt arr=new ArrayInt();
+        int[] array2=new int[]{8,7,2,3,4};
+        int x=arr.searchFrequentElement(array2);
+        assertTrue(x==8||x==7||x==2||x==3||x==4);
+
         int[] array3=new int[]{1,1,8,8,10,10};
-        assertEquals(10,arr.searchFrequentElement(array3));
+        int y=arr.searchFrequentElement(array3);
+        assertTrue(x==1||x==8||x==10);
     }
 
 
+    @Test
+    void testSwitchArray_true(){
+        ArrayInt arr=new ArrayInt();
+        int[] array=new int[]{1,3,1,3,1,3};
+        assertTrue(arr.switchArray(array));
+
+        int[] array1=new int[]{1,3,1,3,1,};
+        assertTrue(arr.switchArray(array1));
+
+        int[] array2=new int[]{};
+        assertTrue(arr.switchArray(array1));
+        int[] array3=new int[]{1};
+        assertTrue(arr.switchArray(array1));
+
+    }
+
+    @Test
+    void testSwitchArray_false(){
+        ArrayInt arr=new ArrayInt();
+
+        int[] array2=new int[]{1,2,1,3};
+        assertFalse(arr.switchArray(array2));
+        int[] array3=new int[]{2,6,1,6};
+        assertFalse(arr.switchArray(array3));
+    }
+    @Test
+    void testMaxSpan(){
+        ArrayInt arr=new ArrayInt();
+
+        int[] array1=new int[]{3};
+        assertEquals(1,arr.maxSpan(array1));
+        int[] array2=new int[]{1, 2,3,4,5};
+        assertEquals(2,arr.maxSpan(array2));
+        int[] array3=new int[]{2,6,1,6,2,3,8,8,9,6};
+        assertEquals(9,arr.maxSpan(array3));
+        int[] array4=new int[]{};
+        assertEquals(0,arr.maxSpan(array4));
+    }
 }
